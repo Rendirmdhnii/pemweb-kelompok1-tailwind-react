34c7930e942840700dff71eb5a4b4d6e638f4461
desbellion Seccar <desbellionsr06@gmail.com>
2026-03-29 00:06:26 +0700

    Update nama lengkap di App.jsx

--- praktikum-react/src/App.jsx
+++ praktikum-react/src/App.jsx
@@ -31,7 +31,7 @@ function App() {
               {/* --- KOTAK JENENG 1 (SOFYAN - KETUA) --- */}
               {/* hover:-translate-y-1 ambek hover:scale-105 gawe animasi ben mantep pas demo rek */}
               <div className="p-3 border rounded-xl bg-blue-50 hover:-translate-y-1 hover:scale-105 hover:shadow-md transition-all duration-300 cursor-pointer">
-                <h5 className="text-lg text-blue-700 font-bold mb-1">Sofyan Muzakki</h5>
+                <h5 className="text-lg text-blue-700 font-bold mb-1">Sufyan Muzakki Hafiansyah</h5>
                 <h6 className="text-xs text-gray-600 font-medium">NIM: 253140707111087</h6>
                 <span className="badge bg-primary mt-1 shadow-sm">Ketua Kelompok</span>
               </div>
@@ -45,7 +45,7 @@ function App() {
 
               {/* --- KOTAK JENENG 3 (KRISNA) --- */}
               <div className="p-3 border rounded-xl bg-gray-50 hover:-translate-y-1 hover:scale-105 hover:shadow-md transition-all duration-300 cursor-pointer">
-                <h5 className="text-lg text-blue-600 font-bold mb-1">Krisna Aji</h5>
+                <h5 className="text-lg text-blue-600 font-bold mb-1">Krisna Aji Dharma Jati</h5>
                 <h6 className="text-xs text-gray-500 font-medium">NIM: 253140707111078</h6>
                 <span className="badge bg-secondary mt-1 shadow-sm">Anggota</span>
               </div>
